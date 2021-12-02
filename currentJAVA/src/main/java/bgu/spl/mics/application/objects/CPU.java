210fec6a657b09cfdb8cfdbdaab8d8901753651a
Yona Solomon <yonahs25@gmail.cpm>
2021-12-02 22:23:29 +0200

    just checking

--- currentJAVA/src/main/java/bgu/spl/mics/application/objects/CPU.java
+++ currentJAVA/src/main/java/bgu/spl/mics/application/objects/CPU.java
@@ -6,4 +6,7 @@ package bgu.spl.mics.application.objects;
  * Add fields and methods to this class as you see fit (including public methods and constructors).
  */
 public class CPU {
+
+    private int currTick;
+    private int startTick;
 }
